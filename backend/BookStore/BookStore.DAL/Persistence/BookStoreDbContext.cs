1f3433b89245705394d463f5c5e8175c46a5e026
BohdanLytvynov <blitvinov19@gmail.com>
2024-08-07 15:39:22 +0300

    Working on Db Infrastructure

---
+++ backend/BookStore/BookStore.DAL/Persistence/BookStoreDbContext.cs
@@ -0,0 +1,22 @@
+﻿using BookStore.DAL.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookStore.DAL.Persistence
+{
+    public class BookStoreDbContext : IdentityDbContext<User, IdentityRole<Guid>, Guid>
+    {
+        #region ctor
+
+        #endregion
+
+        #region For Migration
+
+        #endregion
+    }
+}
